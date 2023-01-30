f1ea4c66392e224a673364f60815f34d51d1654f
Chaithanya Sai Karne <108287884+ckarneupenn@users.noreply.github.com>
2023-01-29 19:36:44 -0500

    Update model.py

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -7,13 +7,21 @@ class LinearRegression:
     b: float
 
     def __init__(self):
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        pass
 
     def fit(self, X, y):
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        if np.linalg.det(X.T@X) != 0:
+          self.w = (np.linalg.inv(X.T@X))@(X.T@y)
+        else:
+          print('Closed form solution not possible here as determinant of X.T@X is not zero')
 
     def predict(self, X):
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        self.b = np.ones((X.shape[0], ))
+        preds = (X@self.w.T) + self.b
+        return preds
 
 
 class GradientDescentLinearRegression(LinearRegression):
@@ -24,7 +32,45 @@ class GradientDescentLinearRegression(LinearRegression):
     def fit(
         self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000
     ) -> None:
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        
+        X = torch.tensor(X)
+        y = torch.tensor(y)
+
+        model = nn.Sequential(nn.Linear(in_features=8, out_features=1).double())
+
+        criterion = nn.MSELoss()
+
+        optimizer = torch.optim.Adam(model.parameters(), lr = lr)
+
+        losses = []
+
+        for i in range(0, epochs):
+
+          preds = model(X)
+          preds = preds.squeeze(1)
+          loss = criterion(preds, y)
+
+          if i%100 == 0:
+            print(f'Loss in {i}th epoch is {loss.item()}')
+
+          losses.append(loss.item())
+          loss.backward()
+          optimizer.step()
+          optimizer.zero_grad()
+
+        print('Final loss is ', losses[-1])
+
+        self.w = model[0].weight.data.squeeze(0)
+        print('The final trained weights are: ', self.w)
+
+        fig, ax = plt.subplots()
+        ax = plt.plot(range(epochs), losses)
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.title('Loss Curve')
+        plt.show
+        
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -37,4 +83,8 @@ class GradientDescentLinearRegression(LinearRegression):
             np.ndarray: The predicted output.
 
         """
-        raise NotImplementedError()
+        # raise NotImplementedError()
+
+        X = torch.tensor(X)
+        preds = X@self.w.T
+        return preds.numpy()
