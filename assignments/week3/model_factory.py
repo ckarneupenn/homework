2f51f26c94666cf0ecefdff9db87ea6dce7ea53e
Chaithanya Sai Karne <ckarne@seas.upenn.edu>
2023-02-04 11:59:09 -0500

    Update model_factory.py

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -13,7 +13,6 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
 
     Returns:
         MLP: The created model.
-
     """
     return MLP(
         input_dim, 40, output_dim, 1, torch.nn.LeakyReLU, torch.nn.init.xavier_uniform_
