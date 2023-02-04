eb078bdfa0128e0f9bda9535cff4d0d7b0b5533e
Chaithanya Sai Karne <ckarne@seas.upenn.edu>
2023-02-03 19:59:03 -0500

    Update model.py

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -59,6 +59,7 @@ class MLP(torch.nn.module):
         Returns:
             The output of the network.
         """
+
         for layer in self.layers:
             x = layer(x)
         x = self.out(x)
