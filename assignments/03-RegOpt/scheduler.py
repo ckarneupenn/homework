a867f5833de2fffa659edbded81632cbf15de006
Chaithanya Sai Karne <ckarne@seas.upenn.edu>
2023-02-19 01:30:46 -0500

    Update scheduler.py

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import numpy as np
 from torch.optim.lr_scheduler import _LRScheduler
 
 
@@ -8,7 +8,6 @@ class CustomLRScheduler(_LRScheduler):
         self,
         optimizer,
         last_epoch=-1,
-        drop=0.77,
         drop_point=0.1,
         initial_learning_rate=0.1,
     ):
@@ -19,24 +18,31 @@ class CustomLRScheduler(_LRScheduler):
 
         """
         # ... Your Code Here ...
-        self.drop = drop
         self.drop_point = drop_point
         self.initial_learning_rate = initial_learning_rate
 
-        super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
+        self.k = 0.00001
+        
+        self.func = lambda x : x * ((np.exp(self.k*self.last_epoch))-0.04)
+
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
+        
     def get_lr(self) -> List[float]:
+        """
+        runs step() on the scheduler
+
+        """
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
         # ... Your Code Here ...
         if self.last_epoch == 0:
-            return [self.initial_learning_rate]
+          return [self.initial_learning_rate]
 
-        if self.last_epoch % 50 == 0:
-            self.initial_learning_rate = self.initial_learning_rate * (self.drop)
-            return [self.initial_learning_rate]
+        if self.last_epoch % self.drop_point == 0:
+          self.initial_learning_rate = self.func(self.initial_learning_rate)
+          return [self.initial_learning_rate]
 
         return [self.initial_learning_rate]
         # Here's our dumb baseline implementation:
