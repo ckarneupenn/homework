72e521e399a9a1d66f05340db22e28eff2bda488
Chaithanya Sai Karne <ckarne@seas.upenn.edu>
2023-02-18 23:13:51 -0500

    Update scheduler.py

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -4,7 +4,7 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    def __init__(self, optimizer, last_epoch=-1, drop=0.77, drop_point=0.1, initial_learning_rate=0.1):
         """
         Create a new scheduler.
 
@@ -13,6 +13,11 @@ class CustomLRScheduler(_LRScheduler):
 
         """
         # ... Your Code Here ...
+        self.drop = drop
+        self.drop_point = drop_point
+        self.initial_learning_rate = initial_learning_rate
+        
+        super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
@@ -20,5 +25,13 @@ class CustomLRScheduler(_LRScheduler):
         # this function (because it is called internally by Torch)
 
         # ... Your Code Here ...
+        if self.last_epoch == 0:
+          return [self.initial_learning_rate]
+
+        if self.last_epoch % 50 == 0:
+          self.initial_learning_rate = self.initial_learning_rate * (self.drop)
+          return [self.initial_learning_rate]
+
+        return [self.initial_learning_rate]
         # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        # return [i for i in self.base_lrs]
