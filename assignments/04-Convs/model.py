ad08721fa547964d7e7d3fb2b620f9e6f48d468b
Chaithanya Sai Karne <ckarne@seas.upenn.edu>
2023-03-12 00:56:07 -0500

    Update model.py

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -13,13 +13,13 @@ class Model(torch.nn.Module):
         super(Model, self).__init__()
 
         self.conv1 = torch.nn.Conv2d(
-            in_channels=num_channels, out_channels=32, kernel_size=3, stride=1
+            in_channels=num_channels, out_channels=16, kernel_size=3, stride=1
         )
-        self.bn1 = torch.nn.BatchNorm2d(num_features=32)
+        self.bn1 = torch.nn.BatchNorm2d(num_features=16)
         self.activation = torch.nn.ReLU()
         self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
         self.dropout = torch.nn.Dropout(p=0.5)
-        self.fc1 = torch.nn.Linear(in_features=7200, out_features=128)
+        self.fc1 = torch.nn.Linear(in_features=3600, out_features=128)
         self.fc2 = torch.nn.Linear(in_features=128, out_features=num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
